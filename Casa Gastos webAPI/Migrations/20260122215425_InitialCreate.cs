851f5356f609e71dca3316e87e7b6c9dfafd85d4
Igor <igorlimasandes@hotmail.com>
2026-01-22 18:57:52 -0300

    feat: adicionando o migrations para o db.

---
+++ Casa Gastos webAPI/Migrations/20260122215425_InitialCreate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Casa_Gastos_webAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
